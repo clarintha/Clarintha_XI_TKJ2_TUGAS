6112820be1d2fa8e3829ce5195d6429f3fe6df1b
clarintha <148990493+clarintha@users.noreply.github.com>
2023-11-08 22:58:32 +0700

    Add files via upload

---
+++ Tugas_Perulangan/Clarintha_Perulangan_5.py
@@ -0,0 +1,9 @@
+#nama: Clarintha Chelsy N
+#Kelas: XI TKJ 2
+#Absen: 07
+#Soal: Sebuah bakteri pembelahan setiap 20 menit. Sebuah bakteri ditempatkan dalam lingkungan yang cocok dan berkembang biak dengan cepat. Buatlah program yang menghitung berapa kali pembelahan bakteri terjadi dalam rentang waktu 2 jam.
+
+menit = 120
+pembelahan_bakteri = menit // 20
+
+print("Jumlah pembelahan bakteri dalam 2 jam:", pembelahan_bakteri, "kali.")
