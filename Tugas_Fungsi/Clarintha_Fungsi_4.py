881a9a8123aecd808918de61a821a3654adef11f
clarintha <148990493+clarintha@users.noreply.github.com>
2023-11-08 22:59:28 +0700

    Add files via upload

---
+++ Tugas_Fungsi/Clarintha_Fungsi_4.py
@@ -0,0 +1,12 @@
+#nama: Clarintha Chelsy N
+#Kelas: XI TKJ 2
+#Absen: 07
+#Soal: Buatlah sebuah fungsi untuk menghitung jumlah digit dari suatu bilangan.
+
+def jumlah_digit(bilangan):
+    return sum(map(int, str(abs(bilangan))))
+
+# Contoh penggunaan
+bilangan_user = int(input("Masukkan bilangan: "))
+hasil_jumlah_digit = jumlah_digit(bilangan_user)
+print(f"Jumlah digit dari {bilangan_user} adalah {hasil_jumlah_digit}.")
